835446679dd018ce1a1045f522c2f5db2d92d185
Akhmanov <epsontiii@gmail.com>
2022-09-03 20:33:11 +0300

    feat: adding new game "calc" and refactoring with new script "logic"

---
+++ brain_games/logic.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import prompt
+
+def logic(game):
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    i = 1
+    while i <= 3:
+        question1, question2, correct_answer = game()
+        print(question1)
+        print(question2)
+        answer = prompt.string('Your answer: ')
+        if answer == correct_answer:
+            print('Correct')
+            i += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+    print(f'Congratilation, {name}!')
+
+def main():
+    logic()
+
+if __name__ == '__main__':
+    main()
+
+
+        
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from brain_games.games.even import brain_even
+from brain_games.games.calc import brain_calc
 from brain_games.logic import logic
 
 def main():
-    logic(brain_even)
+    logic(brain_calc)
 
 if __name__ == '__main__':
     main()
